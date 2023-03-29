2e9bfd6353950bfdf78bfebd69e65fef89cb76d9
Nikita <nikitosiusss@yandex.ru>
2023-03-30 01:21:33 +0300

    Update: all comments on the review are worked out

--- src/issues/Epic.java
+++ src/issues/Epic.java
@@ -11,7 +11,6 @@ public class Epic extends Task {
     public Epic(String name, String description){
         super(name, description);
     }
-
     public Epic(String name, String description, StatusList status, int id){
         super(name, description, status, id);
     }
